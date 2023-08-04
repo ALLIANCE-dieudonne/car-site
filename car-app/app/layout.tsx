c7fa34dfaef02953f8158d534644d4cea43e210c
ALLIANCE-dieudonne <filalliance769@gmail.com>
2023-08-05 00:56:13 +0200

    initial commit

---
+++ car-app/app/layout.tsx
@@ -0,0 +1,24 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import type { Metadata } from "next";
+import Footer from "@/components/Footer";
+
+export const metadata: Metadata = {
+  title: "ALLIANCECOs",
+  description: "created by alliance companies.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar/>
+        {children}
+        <Footer/></body>
+    </html>
+  );
+}
